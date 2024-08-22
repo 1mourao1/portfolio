a403e91b32c13a5924fae996fa60999afe5eaf5d
Alan Mourao <alanmourao0821@gmail.com>
2024-08-22 13:11:01 -0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
-import './App.css'
-import { About } from './components/About'
+import About from "./components/About"
+import Contact from "./components/Contact"
+import Footer from "./components/Footer"
+import Hero from "./components/Hero"
+import Projects from "./components/Projects"
+import Services from "./components/Services"
+import Skills from "./components/Skills"
 
 function App() {
   return (
-    <main>
-      <About />
-    </main >
+    <>
+      <main>
+        <Hero />
+        <About />
+        <Services />
+        <Projects />
+        <Skills />
+        <Contact />
+      </main>
+      <footer>
+        <Footer />
+      </footer>
+    </>
   )
 }
 
